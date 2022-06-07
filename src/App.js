05c0075ba101b44f9e700ba1b9fd3d75f3db20a3
BMWire <bmwemawire@gmail.com>
2022-06-07 16:23:53 +0300

    chore: add routing to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import Docs from './components/Docs';
-
 // Firebase imports
 import { app, database } from './firebaseConfig';
+
+// React Router Dom imports
+import { Routes, Route } from 'react-router-dom';
+
+// Component imports
+import Docs from './components/Docs';
+import EditDocs from './components/EditDocs';
 
 function App()
 {
   return (
-    <Docs database={database} />
+    <Routes>
+      <Route path='/' element={<Docs database={database} />} />
+      <Route path='/edit/:id' element={<EditDocs database={database} />} />
+    </Routes>
   );
 }
 
